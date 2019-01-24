4cfa037beb88125abdc21c2cd50acd88219920f1
Charlie <charles.mulic@gmail.com>
2019-01-24 12:31:39 -0600

    repo tweak changing id type from long to string

--- src/main/java/com/charliemulic/target/myretail/repositories/ProductsRepository.java
+++ src/main/java/com/charliemulic/target/myretail/repositories/ProductsRepository.java
@@ -3,9 +3,5 @@ package com.charliemulic.target.myretail.repositories;
 import com.charliemulic.target.myretail.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.Optional;
-
-public interface ProductsRepository extends MongoRepository<Product, Long> {
-
-    Optional<Product> findById(String id);
+public interface ProductsRepository extends MongoRepository<Product, String> {
 }
